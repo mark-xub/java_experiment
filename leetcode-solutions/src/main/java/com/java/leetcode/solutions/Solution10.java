53c863e6d9132aa6a6801354afe1b31817d5f94b
mark <maybexubin@gmail.com>
2021-04-15 22:52:20 +0800

    solution 10

--- leetcode-solutions/src/main/java/com/java/leetcode/solutions/Solution10.java
+++ leetcode-solutions/src/main/java/com/java/leetcode/solutions/Solution10.java
@@ -6,64 +6,42 @@ package com.java.leetcode.solutions;
  */
 public class Solution10 {
   public boolean isMatch(String s, String p) {
-    if(s == null && p == null) {
-      return true;
-    }
-    if(s != null && p == null) {
-      return false;
-    }
-    if(s == null) {
-      s = "a";
-    } else {
-      s = "a"+s;
-    }
-    p = "a"+p;
     int m = s.length();
     int n = p.length();
-    boolean [][] dp = new boolean[m][n];
+    boolean [][] dp = new boolean[m+1][n+1];
     dp[0][0] = true;
-
-    for(int i = 0; i < m; i++) {
-      for(int j = 1; j < n; j++) {
-        if(p.charAt(j) == '*') {
-          if(j-2>=0 && dp[i][j-2]) {
-            dp[i][j] = true;
-            continue;
-          }
-          if(i-1>=0 && s.charAt(i-1) == s.charAt(i)) {
-            dp[i][j] = true;
-            continue;
-          }
-          if(j-1>=0 && i-1>=0 && p.charAt(j-1) == '.') {
-            dp[i][j] = true;
-            continue;
-          }
+    for(int i = 2; i <= n; i+=2) {
+      if(p.charAt(i-1) == '*') {
+        dp[0][i] = dp[0][i-2];
+      }
+    }
+    for(int i = 1; i <= m; i++) {
+      for(int j = 1; j <= n; j++) {
+        char sc = s.charAt(i-1);
+        char pc = p.charAt(j-1);
+        if(sc == pc || pc == '.') {
+          dp[i][j] = dp[i-1][j-1];
         }
-        else if(p.charAt(j) == '.') {
-          if(i-1>=0 && dp[i-1][j-1]) {
+        else if(pc == '*') {
+          if(dp[i][j-2]) {
             dp[i][j] = true;
-            continue;
           }
-        }
-        else {
-          if(i-1>=0 && j-1>=0
-              && dp[i-1][j-1]
-              && s.charAt(i) == p.charAt(j)) {
-            dp[i][j] = true;
-            continue;
+          else if(sc == p.charAt(j-2) || p.charAt(j-2) == '.'){
+            dp[i][j] = dp[i-1][j];
           }
         }
       }
     }
-    return dp[m-1][n-1];
+    return dp[m][n];
   }
+
+
 
   public static void main(String[] args) {
 //    System.out.println(new Solution10().isMatch(null, "c*a*b*"));
 //    System.out.println(new Solution10().isMatch("ab", ".*"));
 //    System.out.println(new Solution10().isMatch("aab", "c*a*b"));
     System.out.println(new Solution10().isMatch("mississippi", "mis*is*p*."));
-
-
+    System.out.println(new Solution10().isMatch("aaa",   "ab*a*c*a"));
   }
 }
